0331527f1f721eb0534e559f3154c39e7776eb26
ismetsandikci <ismetburaksandikci@gmail.com>
2017-12-06 20:19:06 +0300

    Menüde değişiklikler yapıldı
    Menü çözünürlük sorunu düzeltildi. Menüdeki seçenekler düzeltildi. Text yazıları strings.xml dosyasına kaydedildi.

--- ilacprojesi/app/src/main/java/com/example/lenovo/ilacprojesi/MainActivity.java
+++ ilacprojesi/app/src/main/java/com/example/lenovo/ilacprojesi/MainActivity.java
@@ -80,17 +80,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_anasayfa) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_etkenMaddeIlac) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_endikasyonBilgiIlac) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_cikis) {
+
+        }
+        /*
+        else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
