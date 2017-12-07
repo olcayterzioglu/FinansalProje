aa2894a264cbf0be354c9bfeb952ce63e01c819d
ismetsandikci <ismetburaksandikci@gmail.com>
2017-12-07 22:19:27 +0300

    Menülere Search butonu eklendi

--- ilacprojesi/app/src/main/java/com/example/lenovo/ilacprojesi/etkenmaddeActivity.java
+++ ilacprojesi/app/src/main/java/com/example/lenovo/ilacprojesi/etkenmaddeActivity.java
@@ -69,10 +69,7 @@ public class etkenmaddeActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
