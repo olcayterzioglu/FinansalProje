665f0da3b18c0abaf52c135c8eb9ba3544508d10
barissskaya <bkaya0199@gmail.com>
2017-12-15 23:34:55 +0300

    Veri çekme işleminde değişiklikler ve eklemeler yapıldı
    İlacın bilgileri, prospektüs içerikleri ve ilaca ait firma bilgileri çekildi.  Çekim sırasında oluşan TR karakter hatası giderildi.

--- veritabani/ilac_prospektus_bot/bot.php
+++ veritabani/ilac_prospektus_bot/bot.php
@@ -110,9 +110,10 @@ function getDetails($links)
 {  
 
 	foreach ($links as $link){
+		
 		$url = $link;
 		//$result = getContents('https://www.ilacprospektusu.com/ilac/14/a-nox-fort-550-mg-20-tablet');
-		//echo "<br>" . $url . "<br>";
+		echo "<br>" . $url . "<br>";
 		$result = getContents($url);
 
 
@@ -120,23 +121,31 @@ function getDetails($links)
 		$ilac_bilgisi = "";
 		$firma_bilgisi = "";
 
-		preg_match_all('/<div class=\"prospektus\" id=\"prospektus\">(.*?)<div class=\"ilacesdegerleri\" id=\"ilacesdegerleri\">/s', $result, $prospektus); 
-
-		$ilac_bilgisi = $ilac[1][0]; 
-
-
-		$prospektus = getDetailIlacProspektus($prospektus, 0);  
+		preg_match_all('/<div class=\"prospektus\" id=\"prospektus\">(.*?)<div class=\"ilacesdegerleri\" id=\"ilacesdegerleri\">/s', $result, $prospektus);  
+		preg_match_all('/<div class=\"ilac_bilgileri\">(.*?)<\/div>/s', $result, $ilac);
+		preg_match_all('/<div class=\"firma_bilgileri\">(.*?)<\/div>/s', $result, $firma);   
  
-		//_print($prospektus);
 
-		echo $url."<br>";
+		$ilac_bilgisi = $ilac[1][0];
+		$prospektus = $prospektus[1][0];
+		$firma_bilgisi = $firma[1][0];
+		
+		
+		$prospektus = getDetailIlacProspektus($prospektus, 0); 
+		$ilac_bilgisi = getDetailIlacBilgileri($ilac_bilgisi, 0);
+		$ilac_bilgisi['firma'] = getDetailFirmaBilgisi($firma_bilgisi, 0);
+		
+		//_print($prospektus);
 		 
 	} 
 }
 
 
 function getDetailIlacProspektus($data, $key)
-{ 
+{
+	require_once 'slug.php';
+	$slug = New Slug();
+
 	$data = strip_tags($data, '<h3><strong><br><div>');
 	$data = str_replace(array('<strong>', '</strong>'), array('<h3>', '</h3>'), $data);
 	$data = preg_replace("/<\/?(div|span)[^>]*\>/i", "", $data);
@@ -144,25 +153,80 @@ function getDetailIlacProspektus($data, $key)
 	$data = str_replace('(adsbygoogle = window.adsbygoogle || []).push({});', '', $data);
 
 	preg_match_all('/<h3>(.*?)<\/h3>/s', $data, $basliklar);
-	//_print($basliklar[1]); 
-	
+	//_print($basliklar[1]);
+
+	$baslik = array();
+	foreach ($basliklar[1] as $key => $value){
+		$baslik[$key] = $slug->title($value);
+	}
+
 	$icerikler = "";
 	$data .= '<h3></h3>';
 	preg_match_all('/<\/h3>(.*?)<h3>/s', $data, $icerikler);
 	//_print($icerikler[1]);
 
-	$ddd = array_combine($basliklar[1], $icerikler[1]);
+	$ddd = array_combine($baslik, $icerikler[1]);
 
 	return $ddd;
 }
   
 
 
+
+function getDetailIlacBilgileri($data, $key)
+{
+	$data = strip_tags($data, '<h2><p>');
+	preg_match_all('/<p>(.*?)<\/p>/s', $data, $basliklar);
+	$ilac_bilgisi = array();
+	preg_match('/<h2>(.*?)<\/h2>/s', $data, $baslik);
+	$ilac_bilgisi['ilac_ismi'] = $baslik[1];
+
+	preg_match('/<p class=\"fiyat\">(.*?)<\/p>/s', $data, $fiyat);
+	$ilac_bilgisi['fiyat'] = (isset($fiyat[1]) ? str_replace('Ilaç Fiyatı:', '', $fiyat[1]) : '');
+
+	foreach ($basliklar[1] as $key => $baslik){
+		if (stristr($baslik, 'Barkod Numarası') == true){
+			$ilac_bilgisi['barkod'] = str_replace('Barkod Numarası:', '', $baslik);
+		}
+
+		if (stristr($baslik, 'Etken Maddesi') == true){
+			$ilac_bilgisi['etken_maddesi'] = str_replace('Etken Maddesi:', '', $baslik);
+		}
+	}
+
+	return $ilac_bilgisi;
+}
+
+
+function getDetailFirmaBilgisi($data, $key)
+{
+	$data = strip_tags($data, '<a>');
+	if (preg_match_all("/<a ?.*>(.*)<\/a>/", $data, $links, PREG_PATTERN_ORDER)){
+		$all_hrefs = array_unique($links[1]);
+	}
+
+	return $all_hrefs[0];
+}
+
+
 function getContents($url)
 {
-	$result = file_get_contents($url); 
-	return $result;
-} 
+	$result = file_get_contents($url);
+	$content = iconv('Windows-1252', 'UTF-8', $result);
+	//$content = $result;
+	$content = fixTRCharacter($content);
+
+	return $content;
+}
+
+
+function fixTRCharacter($content)
+{
+	$bad_chr = array('ý', 'Ý', 'þ', 'Þ', 'ð', 'Ð');
+	$new_chr = array('ı', 'I', 'ş', 'Ş', 'ğ', 'Ğ');
+
+	return str_replace($bad_chr, $new_chr, $content);
+}
 ?>  
 </body>
 </html>
